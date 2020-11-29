2d73a56f01bc91a3ad562a57286c8cbd683ee4f0
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-11-29 00:39:45 -0300

    Initial commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace curso_1939_mvc_completo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
